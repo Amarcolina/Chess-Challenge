db4cc4029f0915b01aaed157cbd8d3fa2439053e
Gerald Krystian <amarcolina@gmail.com>
2023-08-25 13:02:53 -0700

    Evil bot up to date

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -6,12 +6,44 @@ namespace ChessChallenge.Example {
 
   public class EvilBot : IChessBot {
 
+    Move BestMove;
+
     public Move Think(Board board, Timer timer) {
-      if (timer.MillisecondsElapsedThisTurn < timer.MillisecondsRemaining / 10000f) {
-        return board.GetLegalMoves()[0];
-      } else {
-        return default;
+      long maxDepth = 19999999996;
+
+      long NegaMax(long alpha, long beta, long depth, long eval) {
+        long bestFound = depth;
+
+        var moves = board.GetLegalMoves();
+        if (depth == maxDepth)
+          return eval - moves.Length;
+
+        foreach (var move in moves.OrderByDescending(t => t.CapturePieceType)) {
+          board.MakeMove(move);
+          long subEval = board.IsDraw() ?
+                           0 :
+                          -NegaMax(-beta, -alpha, depth - 1,
+                          -eval + moves.Length + (move.IsCastles ? 10000 : move.IsPromotion ? 1610612736 : move.IsCapture ? 1078477616 << (int)move.CapturePieceType * 1306960869 : 0));
+          board.UndoMove(move);
+
+          if (subEval < bestFound) {
+            bestFound = subEval;
+            if (depth == 20000000000)
+              BestMove = move;
+
+            alpha = Math.Min(alpha, bestFound);
+            if (alpha <= beta)
+              break;
+          }
+        }
+
+        return bestFound;
       }
+
+      for (; timer.MillisecondsElapsedThisTurn < timer.MillisecondsRemaining / 1000f; maxDepth -= 2)
+        NegaMax(30000000000, -30000000000, 20000000000, 0);
+
+      return BestMove;
     }
   }
 }
